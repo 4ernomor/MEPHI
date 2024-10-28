a04563d8f24d4e000e774173312b2026ea7bf470
Anatoly <anatoly199654@gmail.com>
2024-10-28 23:04:37 +0300

    Create new branch with Scanner

--- src/task473.java
+++ src/task473.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class task473 {
     public static void main(String[] args) {
-        String number = "12345678546";
+        System.out.println("Enter the nums");
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
 
         String formatted = number.replaceAll("(\\d)(?=(\\d{3})+$)", "$1,");
         System.out.println(formatted);
